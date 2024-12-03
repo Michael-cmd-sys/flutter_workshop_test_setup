6d9427f4b6d2db62d04eebde00404966f5548719
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2024-12-03 03:32:29 +0000

    add new page

--- lib/main.dart
+++ lib/main.dart
@@ -184,3 +184,30 @@ class BigCard extends StatelessWidget {
     );
   }
 }
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    } else {
+      return ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text("You have ${appState.favorites.length} favourites saved."),
+          ),
+          for (var pair in appState.favorites)
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text(pair.asLowerCase),
+            ),
+        ],
+      );
+    }
+  }
+}
